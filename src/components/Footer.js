3f0d66c3a6c3d57e8b50dba26527c05d3451fe84
agashvaratharasa <akash@vitalmasks.lk>
2024-09-20 13:43:56 +0530

    changes:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,67 +16,58 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center mb-8">
           <div className="text-center md:text-left mb-4 md:mb-0">
-            <h2 className="text-lg font-semibold">
-              Because water should be free
-            </h2>
-            <p className="mt-2">
-              Help us spread the word and share our movement with your friends
-              and family
-            </p>
-            <button className="mt-4 px-6 py-2 text-white rounded-full bg-[#54C6CD]">
-              Contact Us
-            </button>
+            <a href="/contactus">
+              <button className="mt-4 px-6 py-2 text-white rounded-full bg-[#54C6CD]">
+                Contact Us
+              </button>
+            </a>
           </div>
           <div className="flex space-x-4 mt-4 md:mt-0">
-            <a href="https://tiktok.com" target="_blank" rel="noreferrer">
+            <a
+              href="https://www.tiktok.com/@free_h2o.lk?_t=8ps9StqSAn2&_r=1"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FontAwesomeIcon icon={faTiktok} className="h-6 w-6" />
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <a
+              href="https://www.instagram.com/free_h2o.lk/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
-            </a>
-            <a href="https://facebook.com" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faFacebookF} className="h-6 w-6" />
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faTwitter} className="h-6 w-6" />
-            </a>
-            <a href="https://youtube.com" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faYoutube} className="h-6 w-6" />
-            </a>
-            <a href="https://reddit.com" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faRedditAlien} className="h-6 w-6" />
-            </a>
-            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faLinkedinIn} className="h-6 w-6" />
             </a>
           </div>
         </div>
 
         <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left mb-8">
           <div className="flex flex-col md:flex-row mb-4 md:mb-0">
-            <a href="#" className="block md:inline-block md:mr-4 mb-2 md:mb-0">
-              Contact
+            <a
+              href="/advertise"
+              className="block md:inline-block md:mr-4 mb-2 md:mb-0"
+            >
+              Advertise
             </a>
-            <a href="#" className="block md:inline-block md:mr-4 mb-2 md:mb-0">
-              Projects
-            </a>
-            <a href="#" className="block md:inline-block md:mr-4 mb-2 md:mb-0">
-              Shop Apparel
+            <a
+              href="/distribute"
+              className="block md:inline-block md:mr-4 mb-2 md:mb-0"
+            >
+              Distribute
             </a>
           </div>
-          <div className="flex flex-col md:flex-row mb-4 md:mb-0">
+          {/* <div className="flex flex-col md:flex-row mb-4 md:mb-0">
             <a href="#" className="block md:inline-block md:mr-4 mb-2 md:mb-0">
               Order FreeWater Boxes
             </a>
             <a href="#" className="block md:inline-block">
               Order FreeWater Bottles
             </a>
-          </div>
+          </div> */}
         </div>
 
         <div className="text-center">
           <p className="text-sm">
-            &copy; 2024 Copyright FreeWater All Rights Reserved
+            &copy; 2024 Copyright FreeH2O All Rights Reserved
           </p>
         </div>
       </div>
